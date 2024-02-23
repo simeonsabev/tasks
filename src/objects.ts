cc65522b5e5b3b2dec24d5efb0e541cc7d1e1e84
simeonsabev <simeonsb@udel.edu>
2024-02-22 19:18:39 -0500

    objects

--- src/objects.ts
+++ src/objects.ts
@@ -42,11 +42,13 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
+    let boolean = false;
     if (question.type === "short_answer_question") {
-        return question.type === "short_answer_question";
+        boolean = true;
     } else if (question.type === "multiple_choice_question") {
-        return question.options.includes(answer);
+        boolean = question.options.includes(answer);
     }
+    return boolean;
 }
 
 /**
